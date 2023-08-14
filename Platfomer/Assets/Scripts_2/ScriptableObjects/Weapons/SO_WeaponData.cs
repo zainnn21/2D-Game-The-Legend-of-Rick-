ea6a44421fd534a95300643289794c74952b2e68
Zain <abibz97@gmail.com>
2023-08-15 01:10:50 +0700

    delete unused

--- Platfomer/Assets/Scripts_2/ScriptableObjects/Weapons/SO_WeaponData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "newWeaponData", menuName = "Data/Weapon Data/Weapon")]
-public class SO_WeaponData : ScriptableObject
-{
-    public float[] movementSpeed;
-}
